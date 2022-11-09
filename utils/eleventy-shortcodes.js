973e1fa99e7971f69bb6c1993e15bdf999b5afde
Michael Proulx <proulx.mick@gmail.com>
2022-11-09 14:37:08 -0500

    Add Automatic Anchor on Markdown Titles

--- utils/eleventy-shortcodes.js
+++ utils/eleventy-shortcodes.js
@@ -5,14 +5,6 @@ const md = require('markdown-it')({
 
 module.exports = (config) => {
   config.addShortcode('debug', (content) => `<pre>${inspect(content, { depth: 10})}</pre>`);
-
-  config.addShortcode('youtube', (id) => `<div class="video">
-    <div class="video-container">
-      <iframe width="640" height="360" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe>
-    </div>
-  </div>`);
-
-  config.addPairedShortcode('snippet', (content, icon) => `<div class="snippet">
-    ${icon ?? ''}<div>${md.render(content)}</div>
-  </div>`);
+  config.addShortcode('youtube', (id) => `<div class="video"><div class="video-container"><iframe width="640" height="360" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe></div></div>`);
+  config.addPairedShortcode('snippet', (content, icon) => `<div class="snippet">${icon ?? ''}<div>${md.render(content)}</div></div>`);
 }

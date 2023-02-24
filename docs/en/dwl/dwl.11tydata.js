1bb22ddf32e425838d5b92383b6eded11ce91b27
Christine Breault <85639325+DevolutionsChristine@users.noreply.github.com>
2023-02-24 13:31:56 -0500

    Windows EN Tree index

--- docs/en/dwl/dwl.11tydata.js
+++ docs/en/dwl/dwl.11tydata.js
@@ -8,6 +8,7 @@ module.exports = () => ({
   permalink: '{{ (page.filePathStem.replace("dwl/", tree + "/dwl/") if tree !== "dwl" else page.filePathStem) | localized }}/',
   trees: [
     'hub',
-    'server'
+    'server',
+    'rdm'
   ]
 });
